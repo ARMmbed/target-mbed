13a7cb861d869e455fb9aaed0d0f1b959e063bff
James Crosby <James.Crosby@arm.com>
2016-01-26 16:22:35 +0000

    move files from mbed-armcc into place for merged target, and merge shared files

--- CMake/Platform/mbedOS.cmake
+++ CMake/Platform/mbedOS.cmake
@@ -1,4 +1,4 @@
-# Copyright (C) 2014-2015 ARM Limited. All rights reserved. 
+# Copyright (C) 2014-2016 ARM Limited. All rights reserved. 
 
 # This is a platform definition file for mbedOS there isn't much here because
 # most of our setup is related to the compiler used
@@ -7,12 +7,19 @@
 # No shared libraries
 set_property(GLOBAL PROPERTY TARGET_SUPPORTS_SHARED_LIBS FALSE)
 
-set(CMAKE_STATIC_LIBRARY_PREFIX "")
-set(CMAKE_STATIC_LIBRARY_SUFFIX ".a")
 set(CMAKE_EXECUTABLE_SUFFIX "")
-set(CMAKE_C_OUTPUT_EXTENSION ".o")
-set(CMAKE_ASM_OUTPUT_EXTENSION ".o")
-set(CMAKE_CXX_OUTPUT_EXTENSION ".o")
+set(CMAKE_STATIC_LIBRARY_PREFIX "")
+
+if(YOTTA_CFG_MBED_TOOLCHAIN STREQUAL "armcc")
+    set(CMAKE_STATIC_LIBRARY_SUFFIX ".ar")
+elseif(YOTTA_CFG_MBED_TOOLCHAIN STREQUAL "gcc")
+    set(CMAKE_STATIC_LIBRARY_SUFFIX ".a")
+    set(CMAKE_C_OUTPUT_EXTENSION ".o")
+    set(CMAKE_ASM_OUTPUT_EXTENSION ".o")
+    set(CMAKE_CXX_OUTPUT_EXTENSION ".o")
+else()
+    message(FATAL_ERROR "yotta config value mbed.toolchain must be set to either 'armcc' or 'gcc'")
+endif()
 
 include_directories("${CMAKE_BINARY_DIR}/generated/include")
 file(MAKE_DIRECTORY "${CMAKE_BINARY_DIR}/generated/include")
